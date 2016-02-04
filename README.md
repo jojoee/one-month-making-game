b46d7806296a48b96cbd8137f9e00a2f133ef0e4
Nathachai Thongniran <inid3a@gmail.com>
2016-02-04 10:26:22 +0700

    Add all slides

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# one-month-making-game
-a slide of my experience about making a game in one month (and Global Game Jam 2016)
+# One Month Making Game
+A slide of my experience about making a game in one month (and Global Game Jam 2016 - Thailand's site)
+
+## Note
+- Based on: my experience
+- Presentation Library: [spectacle](https://github.com/FormidableLabs/spectacle), [spectacle-boilerplate](https://github.com/FormidableLabs/spectacle-boilerplate)
+- Presentation Library (alternatives) : [impress.js](https://github.com/impress/impress.js), [reveal.js](https://github.com/hakimel/reveal.js), [bespoke](https://github.com/bespokejs/bespoke)
+- Image by: [ThaiGameJam](https://www.facebook.com/ThaiGameJam)
+
+## Controls
+
+|Key Combination|Function|
+|---|---|
+|Right Arrow|Next Slide|
+|Left Arrow|Previous Slide|
+|Space|Next Slide|
+|Shift+Space|Previous Slide|
+|Alt/Option + O|Toggle Overview Mode|
+|Alt/Option + P|Toggle Presenter Mode|
+
+## Game
+- [Play online](http://jojoee.github.io/jam-2016/)
+- [Source code](https://github.com/jojoee/jam-2016)
+- [Game play video](https://www.youtube.com/watch?v=wUZONN4H3DI)
+- [Fanpage](https://www.facebook.com/KiKi-The-Sacrifice-469973519870300/)
+- [Global game jam page](http://globalgamejam.org/2016/games/kiki-sacrifice)
+
+## Getting Started
+1. Set path (e.g. `cd C:\xampp\htdocs\jojoee.com\one-month-making-game`)
+2. Install dependencies `npm install`
+3. Start `npm start`
--- presentation/index.js
+++ presentation/index.js
@@ -28,19 +28,17 @@ import preloader from "spectacle/lib/utils/preloader";
 // Import theme
 import createTheme from "spectacle/lib/themes/default";
 
-// Import custom component
-import Interactive from "../assets/interactive";
-
 // Require CSS
 require("normalize.css");
 require("spectacle/lib/themes/default/index.css");
 
-
 const images = {
-  city: require("../assets/city.jpg"),
-  kat: require("../assets/kat.png"),
-  logo: require("../assets/formidable-logo.svg"),
-  markdown: require("../assets/markdown.png")
+  ggj2016: require("../assets/ggj2016.jpg"),
+  realpc: require("../assets/realpc.jpg"),
+  live1: require("../assets/live1.jpg"),
+  live2: require("../assets/live2.jpg"),
+  ourteam: require("../assets/ourteam.jpg"),
+  ourgame: require("../assets/ourgame.png")
 };
 
 preloader(images);
@@ -53,117 +51,165 @@ export default class Presentation extends React.Component {
   render() {
     return (
       <Spectacle theme={theme}>
-        <Deck transition={["zoom", "slide"]} transitionDuration={500}>
-          <Slide transition={["zoom"]} bgColor="primary">
-            <Heading size={1} fit caps lineHeight={1} textColor="black">
-              Spectacle
-            </Heading>
-            <Heading size={1} fit caps>
-              A ReactJS Presentation Library
-            </Heading>
-            <Heading size={1} fit caps textColor="black">
-              Where You Can Write Your Decks In JSX
-            </Heading>
-            <Link href="https://github.com/FormidableLabs/spectacle">
-              <Text bold caps textColor="tertiary">View on Github</Text>
-            </Link>
-            <Text textSize="1.5em" margin="20px 0px 0px" bold>Hit Your Right Arrow To Begin!</Text>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="black" notes="You can even put notes on your slide. How awesome is that?">
-            <Image src={images.kat.replace("/", "")} margin="0px auto 40px" height="293px"/>
-            <Heading size={2} caps fit textColor="primary" textFont="primary">
-              Wait what?
+        <Deck transition={["slide"]}>
+          <Slide transition={["slide"]} bgColor="primary">
+            <Heading size={1} fit caps lineHeight={1}>
+              One month<br />
+              Making game
             </Heading>
           </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary" notes="<ul><li>talk about that</li><li>and that</li></ul>">
-            <CodePane
-              lang="jsx"
-              source={require("raw!../assets/deck.example")}
-              margin="20px auto"
-            />
-          </Slide>
-          <Slide transition={["slide"]} bgImage={images.city.replace("/", "")} bgDarken={0.75}>
-            <Appear fid="1">
-              <Heading size={1} caps fit textColor="primary">
-                Full Width
-              </Heading>
-            </Appear>
-            <Appear fid="2">
-              <Heading size={1} caps fit textColor="tertiary">
-                Adjustable Darkness
-              </Heading>
-            </Appear>
-            <Appear fid="3">
-              <Heading size={1} caps fit textColor="primary">
-                Background Imagery
-              </Heading>
-            </Appear>
-          </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary">
-            <Heading caps fit>Flexible Layouts</Heading>
-            <Layout>
-              <Fill>
-                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Left
-                </Heading>
-              </Fill>
-              <Fill>
-                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Right
-                </Heading>
-              </Fill>
-            </Layout>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="black">
-            <BlockQuote>
-              <Quote>Wonderfully formatted quotes</Quote>
-              <Cite>Ken Wheeler</Cite>
-            </BlockQuote>
-          </Slide>
-          <Slide transition={["spin", "zoom"]} bgColor="tertiary">
-            <Heading caps fit size={1} textColor="primary">
-              Inline Markdown
-            </Heading>
-            <Markdown>
-              {`
-![Markdown Logo](${images.markdown.replace("/", "")})
 
-You can write inline images, [Markdown Links](http://commonmark.org), paragraph text and most other markdown syntax
-* Lists too!
-* With ~~strikethrough~~ and _italic_
-* And lets not forget **bold**
-              `}
-            </Markdown>
-          </Slide>
-          <Slide transition={["slide", "spin"]} bgColor="primary">
-            <Heading caps fit size={1} textColor="tertiary">
-              Smooth
+          <Slide transition={["slide"]} bgColor="primary">
+            <Heading size={1} fit caps lineHeight={1}>
+              This is my experience
             </Heading>
-            <Heading caps fit size={1} textColor="secondary">
-              Combinable Transitions
-            </Heading>
+            <Text textSize="1.5em" margin="20px 0px 0px" bold caps>not the proper way</Text>
+            <Text textSize="1.5em" margin="20px 0px 0px" bold caps>not the best way</Text>
           </Slide>
-          <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+
+          <Slide transition={["slide"]} bgColor="black"
+            notes="Yea, we making game, we need a TOOL.
+              When you search, you will found something likes,
+              It's depend on
+              - Game platform
+              - Language you familiar with
+              - 2D / 3D
+              - Your dev OS
+              - Etc.">
+            <Heading size={2} fit caps lineHeight={1} textColor="primary">
+              Definitely
+            </Heading>
+            <Text textSize="1.5em" margin="20px 0px 0px" textColor="white" bold caps>we have to pick a TOOL</Text>
             <List>
-              <Appear><ListItem>Inline style based theme system</ListItem></Appear>
-              <Appear><ListItem>Autofit text</ListItem></Appear>
-              <Appear><ListItem>Flexbox layout system</ListItem></Appear>
-              <Appear><ListItem>React-Router navigation</ListItem></Appear>
-              <Appear><ListItem>PDF export</ListItem></Appear>
-              <Appear><ListItem>And...</ListItem></Appear>
+              <ListItem textColor="white">Cross platform</ListItem>
+              <ListItem textColor="white">Community</ListItem>
+              <List>
+                <ListItem margin="0 0 0 60px" textColor="white">more tuts / examples</ListItem>
+                <ListItem margin="0 0 0 60px" textColor="white">more plugins</ListItem>
+                <ListItem margin="0 0 0 60px" textColor="white">more help / support</ListItem>
+              </List>
             </List>
           </Slide>
-          <Slide transition={["slide"]} bgColor="primary">
-            <Heading size={1} caps fit textColor="tertiary">
-              Your presentations are interactive
-            </Heading>
-            <Interactive/>
+
+          <Slide transition={["slide"]} bgColor="black">
+            <Text textSize="3em" margin="20px 0px 0px" textColor="white" bold caps>Unity</Text>
+            <Text textSize="3em" margin="20px 0px 0px" textColor="primary" bold caps>Phaser</Text>
+            <Appear>
+              <List>
+                <ListItem textColor="primary">I know the workflow (by gulp.js)</ListItem>
+                <ListItem textColor="primary">I am familiar with</ListItem>
+                <ListItem textColor="primary">I know a lot of js plugins</ListItem>
+              </List>
+            </Appear>
           </Slide>
-          <Slide transition={["spin", "slide"]} bgColor="tertiary">
-            <Heading size={1} caps fit lineHeight={1.5} textColor="primary">
-              Made with love in Seattle by
+
+          <Slide transition={["slide"]} bgColor="white"
+            notes="In my opinion,
+              writing your own game spent a lot of time
+              Finally, you will see a lot of patterns">
+            <Heading size={1} fit caps lineHeight={1} textColor="primary">
+              How I learn
             </Heading>
-            <Link href="http://www.formidablelabs.com"><Image width="100%" src={images.logo}/></Link>
+            <List>
+              <ListItem textColor="black">See a lot of tuts / examples</ListItem>
+              <ListItem textColor="black">Try to debug / implement / fix</ListItem>
+              <ListItem textColor="black">Not write your own game</ListItem>
+            </List>
+          </Slide>
+
+          <Slide transition={["slide"]} bgColor="white">
+            <Text textSize="2em" textColor="primary" bold>
+              Last day before GGJ2016
+            </Text>
+            <Text textSize="3em" margin="20px 0px 0px" textColor="primary" bold>
+              I got ~40 kind of games
+            </Text>
+          </Slide>
+
+          <Slide transition={["slide"]} bgImage={images.ggj2016.replace("/", "")} bgDarken={0.75}>
+            <Text textSize="3em" textColor="white" bold caps>
+              Global Game Jam
+            </Text>
+
+            <Text textSize="2em" margin="20px 0px 0px" textColor="white" bold caps>
+              (GGJ2016)
+            </Text>
+
+            <Text textSize="1em" margin="40px 0px 0px" textColor="white" bold>
+              TLTR; Making game within 2 days
+            </Text>
+          </Slide>
+
+          <Slide transition={["slide"]} bgColor="white"
+            notes="When I decide to participate this event,
+              I not found anythings about Thailand's site
+              - How they live / code / eat / sleep
+              
+              So, all next slides will be something that I found on this event.
+              Maybe, it can help you to decide to joining this event or not.">
+            <Heading size={2} caps fit textColor="primary">
+              Why attend ?
+            </Heading>
+
+            <Text textSize="2em" margin="20px 0px 0px" textColor="primary" bold>
+              - It force me to make it
+            </Text>
+          </Slide>
+
+          <Slide transition={["slide"]} bgColor="black"
+            notes="Before the last point.
+              So, you need to prepare your team.
+              At least 1 coder, 1 designer">
+            <List>
+              <ListItem textColor="primary">You have to had some experience</ListItem>
+              <ListItem textColor="primary">Laptop is not enough</ListItem>
+              <ListItem textColor="primary">1 monitor is not enough</ListItem>
+              <ListItem textColor="primary">3% foreigner</ListItem>
+              <ListItem textColor="primary">90% use Unity</ListItem>
+              <ListItem textColor="primary">90% already have a team</ListItem>
+              <ListItem textColor="primary">Thailands site has popular vote award</ListItem>
+            </List>
+          </Slide>
+
+          <Slide transition={["slide"]} bgColor="black">
+            <Image src={images.realpc.replace("/", "")} margin="0px auto" height="600px"/>
+            <Text textSize="2em" margin="20px 0px 0px" textColor="primary" bold>
+              40% use PC
+            </Text>
+          </Slide>
+
+          <Slide transition={["slide"]} bgColor="black"
+            notes="So, you should arrive ~14.00 to pick a good area
+              cause you will sleep / eat / code at your area">
+            <Layout>
+              <Fill>
+                <Image src={images.live1.replace("/", "")} margin="0px auto 40px" height="400px"/>
+              </Fill>
+              <Fill>
+                <Image src={images.live2.replace("/", "")} margin="0px auto 40px" height="400px"/>
+              </Fill>
+            </Layout>
+            <Text textSize="2em" margin="20px 0px 0px" textColor="primary" bold>
+              How they 
+            </Text>
+            <Text textSize="2em" margin="20px 0px 0px" textColor="primary" bold>
+              live / code / eat / sleep
+            </Text>
+          </Slide>
+
+          <Slide transition={["slide"]} bgColor="primary">
+            <Image src={images.ourteam.replace("/", "")} margin="0px auto 40px" height="400px"/>
+            <Text textSize="2em" margin="20px 0px 0px" textColor="white" bold>
+              Our Team
+            </Text>
+          </Slide>
+
+          <Slide transition={["slide"]} bgColor="primary"
+            notes="This is all my experiences, thank you">
+            <Image src={images.ourgame.replace("/", "")} margin="0px auto 40px" height="400px"/>
+            <Text textSize="2em" margin="20px 0px 0px" textColor="white" bold>
+              Our Game
+            </Text>
           </Slide>
         </Deck>
       </Spectacle>
